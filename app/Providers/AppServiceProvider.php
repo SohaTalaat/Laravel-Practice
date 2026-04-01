3aacc1488cd0a2fbe44677478b477a010f6a532b
Soha <saha.20350436@science.aun.edu.eg>
2026-04-01 20:37:41 +0200

    Example on contextual and Tagging binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Contracts\Notifier;
+use App\Http\Controllers\OrderController;
 use App\Services\EmailNotifier;
+use App\Services\SmsNotifier;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(Notifier::class, EmailNotifier::class);
+
+        // Interface
+        $this->app->bind(EmailNotifier::class, SmsNotifier::class);
+
+        //Contextual binding
+        $this->app->when(OrderController::class)
+            ->needs(Notifier::class)
+            ->give(SmsNotifier::class);
+
+        //Tagging
+        $this->app->bind(EmailNotifier::class);
+        $this->app->bind(SmsNotifier::class);
+
+        $this->app->tag([EmailNotifier::class, SmsNotifier::class], 'notifiers');
     }
 
     /**
